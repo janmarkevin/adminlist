bb97407c6fb0dd0768adf6db777fb08b39e62e46
JanmarKevin-Espina <janmarkespina.yns@gmail.com>
2021-10-14 17:32:26 +0800

    menu - X transate

--- dist/js/click.js
+++ dist/js/click.js
@@ -1,9 +1,18 @@
 let menu = document.querySelector('.header__menu-wrapper');
+let menuOpen = false;
 let menuContent = document.querySelector('.header__menu-nav');
 menu.addEventListener('click',()=>{
+   if (!menuOpen) {
+      menu.classList.add('open');
+      menuOpen = true;
+   } else {
+      menu.classList.remove('open');
+      menuOpen = false;
+   }
+
    if(menuContent.style.display===""){
       menuContent.style.display="block";
    } else {
       menuContent.style.display="";
    }
-})
+});
